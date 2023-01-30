ab013c7309f062cbf61b2fdf3f3e591f9b45f40a
shivamanand-dev <shivam_anand@outlook.com>
2023-01-30 13:12:30 +0530

    initialized

---
+++ src/Theme/theme.js
@@ -0,0 +1,25 @@
+export const COLORS = {
+  DARK_BACKGROUND: "#3c3c3c",
+  LIGHT_TEXT_COLOR: "#eeeeee",
+};
+export const error = {
+  background: "#F80000",
+  color: "#F80000",
+};
+
+export const darkTheme = {
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+};
+
+const theme = {
+  COLORS,
+  error,
+  darkTheme,
+};
+
+export default theme;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
 
+import theme from "src/Theme/theme";
+import { ThemeProvider } from "styled-components";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
