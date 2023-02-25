79b0e8f5d5adb5d3f6e004bcc2b67368e2a31158
shivamanand-dev <shivam_anand@outlook.com>
2023-02-25 22:26:23 +0530

    backend connected

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,26 @@
 import "../styles/globals.css";
 
-import { useEffect } from "react";
+import { NoSsr } from "@mui/material";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "src/Components/LoadingSpinner";
 import { pageService } from "src/services/pageServices";
 import { ThemeProvider } from "styled-components";
 
 import theme from "@/utils/Theme/theme";
 
 export default function App({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false);
   useEffect(() => {
     pageService.getPageData();
+    setMounted(true);
   });
 
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      {" "}
+      <NoSsr>
+        {mounted ? <Component {...pageProps} /> : <LoadingSpinner />}
+      </NoSsr>
     </ThemeProvider>
   );
 }
